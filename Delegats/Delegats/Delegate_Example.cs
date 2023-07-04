32ddca387b5e2f04ef07deb7e672fbd5b056a060
SAMANDAR <dotnetdasturchi@gmail.com>
2023-07-04 16:29:37 +0500

    example code for delegates

---
+++ Delegats/Delegats/Delegate_Example.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegats.Delegats
+{
+    internal class Delegate_Example
+    {
+
+    }
+}
--- Delegats/Program.cs
+++ Delegats/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("SAmandar");
+﻿
+delegate void Printdelegate(string massage);
+delegate void Ispalindrom(int son);
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Printdelegate printdelegate = PrintMessage;
+        printdelegate("SAlom Samandar");
+
+         Ispalindrom ispalindrom_delegate = new Ispalindrom(PalindromSon);
+         ispalindrom_delegate(121);
+    }
+
+     static void PalindromSon(int son)
+    {
+         int r, sum = 0, result;
+        Console.Write("Enter the Number: ");
+        son = int.Parse(Console.ReadLine());
+        result = son;
+        while (son > 0)
+        {
+            r = son % 10;
+            sum = (sum * 10) + r;
+            son = son / 10;
+        }
+        if (result == sum)
+            Console.Write("Number is Palindrome.");
+        else
+            Console.Write("Number is not Palindrome");
+    }
+
+    static void PrintMessage(string message)
+    {
+        Console.WriteLine(message); 
+    }
+}
+    
