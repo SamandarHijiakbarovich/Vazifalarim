41fbda42ad1984d74ab81d7e6688d8b801dfda06
SAMANDAR <dotnetdasturchi@gmail.com>
2023-07-06 16:17:04 +0500

    User modeliga methodlar yozildi

--- Linq_Asoslari/Models/User.cs
+++ Linq_Asoslari/Models/User.cs
@@ -15,6 +15,25 @@ namespace Linq_Asoslari.Models
         public int CarId { get; set; }
         public string PhonNumber { get; set; }
 
+        public bool Equals(User user1,User user2)
+        {
+            return (user1.FirstName == user2.FirstName &&
+                user1.LastName == user2.LastName && 
+                user1.PhonNumber == user2.PhonNumber);
+        }
+
+        public int GetHashCode(User obj)
+        {
+            if(obj is null)
+            {
+                return 0;
+               
+            }
+            int IdHashCode = obj.Id.GetHashCode();
+            int NameHashCode = obj.FirstName is null ? 0 : obj.FirstName.GetHashCode();
+            return IdHashCode ^ NameHashCode;
+        }
+
 
     }
 }
--- Linq_Asoslari/Program.cs
+++ Linq_Asoslari/Program.cs
@@ -7,5 +7,11 @@ IList<User> users = new List<User>()
     new User(){Id = 2,FirstName="Sanjar",LastName="soatov",Age=14,CarId=3,PhonNumber="942647610"},
 
 };
-var result = users.OrderBy(x=>x.LastName).ThenBy(p=>p.FirstName).ThenBy(p=>p.Age).ToList();
-result.ForEach(d => Console.WriteLine($"{d.LastName}|{d.FirstName}|{d.Age}"));
+//var result = users.OrderBy(x=>x.LastName).ThenBy(p=>p.FirstName).ThenBy(p=>p.Age).ToList();
+//result.ForEach(d => Console.WriteLine($"{d.LastName}|{d.FirstName}|{d.Age}"));
+
+//var result =users.GroupBy(p=>p.PhonNumber).Select(p=>p.First()).ToList();
+//result.ForEach(p=>Console.WriteLine(p.LastName));
+
+var natija = users.Distinct().ToList();
+natija.ForEach(x=>Console.WriteLine(x.FirstName));  
